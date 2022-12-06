5ad524e4b6e0419ca0dda867898abd85ec786831
Sirb Maria <sirbmaria27@gmail.com>
2022-12-06 21:19:55 +0200

    Lab8

--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -3,11 +3,20 @@ namespace Sirb_Maria_Lab7;
 
 public partial class ListPage : ContentPage
 {
-	public ListPage()
+    public ListPage()
 	{
 		InitializeComponent();
 	}
 
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ProductPage((ShopList)
+       this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
     async void OnSaveButtonClicked(object sender, EventArgs e)
     {
         var slist = (ShopList)BindingContext;
@@ -21,4 +30,13 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
